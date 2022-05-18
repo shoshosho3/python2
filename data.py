71e39bdec928a32ecfbd13c1ffca0b9bc73c0744
shoshosho3 <benhellman80@gmail.com>
2022-05-19 01:03:31 +0300

    COMMIT

--- data.py
+++ data.py
@@ -9,6 +9,7 @@ def load_data(path):
 
 def add_new_columns(df):
     df["season_name"] = df["season"].apply(get_name)
+    df["Hour"] = df["timestamp"].apply(lambda x: int(datetime.strptime(x, "%d/%m/%Y %H:%M").strftime("%H")))
     df["Day"] = df["timestamp"].apply(lambda x: int(datetime.strptime(x, "%d/%m/%Y %H:%M").strftime("%d")))
     df["Month"] = df["timestamp"].apply(lambda x: int(datetime.strptime(x, "%d/%m/%Y %H:%M").strftime("%m")))
     df["Year"] = df["timestamp"].apply(lambda x: int(datetime.strptime(x, "%d/%m/%Y %H:%M").strftime("%Y")))
@@ -38,10 +39,18 @@ def data_analysis(df):
                        for col1 in corr.columns for col2 in corr.columns[corr.columns.get_loc(col1) + 1:] if
                        corr[col1][col2]}
     sorted_dictionary = sorted(corr_dictionary.items(), key=lambda x: x[1])
-    print("Highest correlated are:")
+    print("Highest correlated are: ")
     dic_len = len(sorted_dictionary)
     for i in range(5):
-        print(f"{i}. {sorted_dictionary[dic_len - i - 1][0]} with {sorted_dictionary[dic_len - i - 1][1]}")
-    print("Lowest correlated are:")
+        print(f"{i+1}. {sorted_dictionary[dic_len - i - 1][0]} with {sorted_dictionary[dic_len - i - 1][1]}")
+    print("Lowest correlated are: ")
     for i in range(5):
-        print(f"{i}. {sorted_dictionary[i][0]} with {sorted_dictionary[i][1]}")
+        print(f"{i+1}. {sorted_dictionary[i][0]} with {sorted_dictionary[i][1]}")
+
+    group = df[["season_name", "t_diff"]].groupby(['season_name']).mean().to_dict()
+    for item1 in group.items():
+        for key, value in item1[1].items():
+            val = "%.2f" % value
+            print(f"{key} average t_diff is {val}")
+    all_mean = "%.2f" % df[["t_diff"]].mean().to_dict()["t_diff"]
+    print(f"All average t_diff is {all_mean}")
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import data
 
 if __name__ == '__main__':
+    print("Part A: ")
     df_all = data.load_data("hw2/london_sample_500.csv")
     data.add_new_columns(df_all)
     data.data_analysis(df_all)
